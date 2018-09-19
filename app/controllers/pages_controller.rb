7659b7e1737e534ae1f5e0c873b871ed3e1289f3
Jesse Davis <jdavis93@live.com>
2018-09-19 09:36:40 -0600

    pages are now setup

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,17 @@
+class PagesController < ApplicationController
+
+  def home
+  end
+
+  def music
+  end
+
+  def shows
+  end
+
+  def about
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'music', to: 'pages#music'
+  get 'shows', to: 'pages#shows'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+
+  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
